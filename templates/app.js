96a6012be2d148045d38b6154a6b9c8a35595cdd
Glaydston Veloso <glaydston.oliveira@gmail.com>
2014-06-09 14:27:28 -0300

    Introduces in Jade style

---
+++ templates/app.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+
+// These commands are telling Express
+// that all of its views should be drawn
+// from the folder name 'views'..
+app.set('view engine', 'jade')
+app.set('view options', {layout: true})
+app.set('views', express.static(__dirname + '/views'))
+
+// Expecting the name of one of the stooges as input
+app.get('/stooges/:name?', function(req, res){
+  var name = req.params.name
+  
+  switch( name ? name.toLowerCase() : ''){
+    case 'larry':
+    case 'curly':
+    case 'moe':
+      res.render('stooges', {stooge: name})
+      break
+    default:
+      //next()
+   }
+})
+
+
+// A fallback from the previous route, in case the name provided was not found
+app.get('/stooges/*?', function(req, res){
+  res.render('stooges', {stooge: null})
+})
+
+// A default route used to access the application’s home page
+app.get('/?', function(req, res){
+  res.render('index')
+})
+
+var port = 8080
+app.listen(port)
+console.log('Listeng on port ' + port)
