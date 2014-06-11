9279ca9b3d18f73f11e6cb0b130c17f68eb70148
Glaydston Veloso <glaydston.oliveira@gmail.com>
2014-06-11 00:23:01 -0300

    Handling register request and template

--- my-social-network/app.js
+++ my-social-network/app.js
@@ -15,4 +15,19 @@ app.get('account/authenticated', function(req, res){
 		:	res.send(401)
 })
 
+app.post('/register', function(req, res){
+	var firstName = req.param('firstName', '')
+	var lastName = req.param('lastName','')
+	var email = req.param('email', '')
+	var password = req.param('password', '')
+
+	if(null == email || null == password){
+		res.send(400)
+		return
+	}
+
+	Account.register(email, password, firstName, lastName)
+	res.send(200)
+})
+
 app.listen(8080)
