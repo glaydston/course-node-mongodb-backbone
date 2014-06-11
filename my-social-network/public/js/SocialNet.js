5826d441e752c5632113386ebcc6278af1cfcfd8
Glaydston Veloso <glaydston.oliveira@gmail.com>
2014-06-11 00:00:04 -0300

    Changed the 'SocialNet' with checking authentication

--- my-social-network/public/js/SocialNet.js
+++ my-social-network/public/js/SocialNet.js
@@ -1,6 +1,23 @@
-define(['views/index'], function(indexView){
+define(['router'], function(router){
   var initialize = function(){
-    indexView.render()  
+    checkLogin(runApplication)  
+  }
+
+  var checkLogin = function(callback){
+    $.ajax("/account/authenticated", {
+      method: "GET",
+      sucess: function(){
+        return callback(true)
+      },    
+      error: function(){
+        return callback(false)
+      } 
+    })
+  }
+
+  var runApplication = function(authenticated){
+    window.location.hash = !authenticated ? 'login' : 'index'
+    Backbone.history.start()    
   }
 
   return {
